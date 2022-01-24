888e9256ac9d9ced6fc53f94509b68781c9400f0
hanamaze <hanadmx@yahoo.co.id>
2022-01-24 11:57:38 +0700

    siap

--- views/nomor_iso/nomor_iso_form.php
+++ views/nomor_iso/nomor_iso_form.php
@@ -3,6 +3,8 @@
     <head>
         <title>harviacode.com - codeigniter crud generator</title>
         <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
+        <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
+
         <style>
             body{
                 padding: 15px;
@@ -46,16 +48,23 @@
 	    <input type="hidden" name="id_nomoriso" value="<?php echo $id_nomoriso; ?>" /> 
 	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
 	    <a href="<?php echo site_url('nomor_iso') ?>" class="btn btn-default">xCancel</a>
-	</form>
-    </body>
-</html>
 
-<script type="text/javascript">
+
+
+        <script type="text/javascript">
             $(document).ready(function () {
-                $('.tanggal').datepicker({
+                $('.tgl_issued').datepicker({
                     format: "dd-mm-yyyy",
                     autoclose:true
                 });
             });
         </script>
+	</form>
+
+
+    
+    </body>
+</html>
+
+
     
